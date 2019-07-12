528b74de4cf3c86e86ee441b9c3c9400efb00ad3
unknown <yzeed112009@hotmail.com>
2019-07-12 16:11:07 +0300

    Version: v1.0
    Made some final touches!
    
    - Adds extra helpful comment messages
    - ran "pycodestyle" on the code it displays no warnings or errors at all.

--- python/RPS v1.0.py
+++ python/RPS v1.0.py
@@ -20,98 +20,102 @@ class Player:  # Parent Player Template
     def move(self):
         return 'rock'
 
-    
-        
-
-    def learn(self, my_move , their_move): 
+    def learn(self, my_move, their_move):
         pass
 
-class RandomPlayer(Player): # Random Player Behavior Template
+
+class RandomPlayer(Player):  # Random Player Behavior Template
     def move(self):
         return random.choice(moves)
 
-class ReflectPlayer(Player): # First Play is Always a Rock 
+
+class ReflectPlayer(Player):  # First Play is Always a Rock
     def __init__(self):
         Player.__init__(self)
         self.their_move = None
 
     def move(self):
         if self.their_move is None:
-            play = moves[0]               
+            play = moves[0]
         else:
-            play = self.their_move             
-            return (play)  
+            play = self.their_move
+            return (play)
 
     def learn(self, my_move, their_move):
         self.their_move = their_move
 
-class CyclePlayer(Player): # To Make the game more chanllenging!
+
+class CyclePlayer(Player):  # To Make the game more chanllenging!
     def __init__(self):
         Player.__init__(self)
         self.my_move = None
 
     def move(self):
-            play = None
-            if self.my_move is None:
-                play = Player.move(self)
-            else:
-                index = moves.index(self.my_move) + 1
-                if index >= len(moves):
-                    index = 0
-                play = moves[index]
-            self.my_move = play
-            return play   
+        play = None
+        if self.my_move is None:
+            play = Player.move(self)
+        else:
+            index = moves.index(self.my_move) + 1
+            if index >= len(moves):
+                index = 0
+            play = moves[index]
+        self.my_move = play
+        return play
 
-class HumanPlayer(Player): # Human Player Actions
+
+class HumanPlayer(Player):  # Human Player Actions
     def move(self):
         action = input('rock, paper, scissors? >')
         while action not in moves:
             action = input('Sorry try again. >')
         return(action)
 
-def beats(one, two):
+
+def beats(one, two):  # Referee!
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
 
 
-class Game: # Game Engine!
+class Game:  # Game Engine!
     def __init__(self, p1, p2):
         self.p1 = HumanPlayer()
         self.p2 = CyclePlayer()
-    
+
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"You played {move1}")
-        print(f"John played {move2}") #Introducing John The RPS AI!
+        print(f"John played {move2}")  # Introducing John The RPS AI!
         if beats(move1, move2):
-            print ("Victory!")
+            print("Victory!")
             print(f"Score: You: {move1}  John: {move2}\n\n")
             self.p1.score += 1
             return 1
         elif beats(move2, move1):
-            print ("John Wins You Lose!")
+            print("John Wins You Lose!")
             print(f"Score: You: {move1}  John: {move2}\n\n")
             self.p2.score += 1
             return 2
         else:
-            print ("Draw!!")
+            print("Draw!!")
             print(f"Score: You: {move1}  John: {move2}\n\n")
             return 0
 
     def play_game(self):
         try:
-            print("Welcome to Rock, Paper, Scissors CLI minigame!") # Welcome Message
+            # Welcome Message
+            print("Welcome to Rock, Paper, Scissors CLI minigame!")
             time.sleep(1)
-            print("John Challenges You to a game of RPS!") # John Dialogue
+            print("John Challenges You to a game of RPS!")  # John Dialogue
             time.sleep(1)
-            prompt = input("Do You Accept the Challenge?\n".lower() + "Press Y to Accept X to Exit.\n".lower())
-            if prompt is "x":
+            prompt = input("Do You Accept the Challenge?\n" +
+                           "Press Y to Accept X to Exit.\n")
+            if prompt in ("x", "X"):
                 print("Goodbye then!")
                 exit()
             elif prompt is "y":
-                pass    
+                pass
             print("Game start!")
             for round in range(3):
                 print(f"Round {round}:")
@@ -122,11 +126,12 @@ class Game: # Game Engine!
                 print('John Won!')
             else:
                 print('The game was a tie!')
-            print('The final score is: ' + str(self.p1.score) + ' TO ' + 
-             str(self.p2.score))
-            print("Game over!")
-        except KeyboardInterrupt: # Handle errors Like a Beast!
+            print('The final score is: ' + str(self.p1.score) + ' TO ' +
+                  str(self.p2.score))
+            print("Game over! Thanks For Playing!")
+        except KeyboardInterrupt:  # Handle errors Like a Beast!
             print("Thanks For Playing Goodbye!")
+
 
 if __name__ == '__main__':
     game = Game(Player(), Player())
