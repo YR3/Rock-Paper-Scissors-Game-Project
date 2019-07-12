12061335878dce086fba6372e0a969bbc4a6ebb8
unknown <yzeed112009@hotmail.com>
2019-07-12 15:18:13 +0300

    Version: v0.3
    - Adds some helpful comments
    - Introducing John The RPS AI!
    - Adds a Welcome Message
    - Adds some Dialogue and prompt to ask player to play
    - Handles the Ctr + C and gives player a Goodbye message :)

--- python/RPS v0.3.py
+++ python/RPS v0.3.py
@@ -2,6 +2,8 @@
 # Yazeed M. Al`Fadani`s Rock, Paper, Scissor Project
 # For Udacity`s Connect Into to Programming course
 import random
+import time
+import sys
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
@@ -21,14 +23,14 @@ class Player:  # Parent Player Template
     
         
 
-    def learn(self, my_move , their_move):
+    def learn(self, my_move , their_move): 
         pass
 
 class RandomPlayer(Player): # Random Player Behavior Template
     def move(self):
         return random.choice(moves)
 
-class ReflectPlayer(Player): # First Play is Always a Rock
+class ReflectPlayer(Player): # First Play is Always a Rock 
     def __init__(self):
         Player.__init__(self)
         self.their_move = None
@@ -43,7 +45,7 @@ class ReflectPlayer(Player): # First Play is Always a Rock
     def learn(self, my_move, their_move):
         self.their_move = their_move
 
-class CyclePlayer(Player): 
+class CyclePlayer(Player): # To Make the game more chanllenging!
     def __init__(self):
         Player.__init__(self)
         self.my_move = None
@@ -82,7 +84,7 @@ class Game: # Game Engine!
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"You played {move1}")
-        print(f"John played {move2}")
+        print(f"John played {move2}") #Introducing John The RPS AI!
         if beats(move1, move2):
             print ("Victory!")
             print(f"Score: You: {move1}  John: {move2}\n\n")
@@ -99,20 +101,32 @@ class Game: # Game Engine!
             return 0
 
     def play_game(self):
-        print("Game start!")
-        for round in range(3):
-            print(f"Round {round}:")
-            self.play_round()
-        if self.p1.score > self.p2.score:
-            print('You won!')
-        elif self.p1.score < self.p2.score:
-            print('John Won!')
-        else:
-            print('The game was a tie!')
-        print('The final score is: ' + str(self.p1.score) + ' TO ' +
-              str(self.p2.score))
-        print("Game over!")
-
+        try:
+            print("Welcome to Rock, Paper, Scissors CLI minigame!") # Welcome Message
+            time.sleep(1)
+            print("John Challenges You to a game of RPS!") # John Dialogue
+            time.sleep(1)
+            prompt = input("Do You Accept the Challenge?\n".lower() + "Press Y to Accept X to Exit.\n".lower())
+            if prompt is "x":
+                print("Goodbye then!")
+                exit()
+            elif prompt is "y":
+                pass    
+            print("Game start!")
+            for round in range(3):
+                print(f"Round {round}:")
+                self.play_round()
+            if self.p1.score > self.p2.score:
+                print('You won!')
+            elif self.p1.score < self.p2.score:
+                print('John Won!')
+            else:
+                print('The game was a tie!')
+            print('The final score is: ' + str(self.p1.score) + ' TO ' + 
+             str(self.p2.score))
+            print("Game over!")
+        except KeyboardInterrupt: # Handle errors Like a Beast!
+            print("Thanks For Playing Goodbye!")
 
 if __name__ == '__main__':
     game = Game(Player(), Player())
